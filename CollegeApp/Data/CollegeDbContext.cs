633a9f7d39e7128094ba520316866159980fcc46
Mehdi Heidari <hymehdi84@gmail.com>
2025-10-16 00:37:18 +0200

    first two rows added to database

--- CollegeApp/Data/CollegeDbContext.cs
+++ CollegeApp/Data/CollegeDbContext.cs
@@ -4,10 +4,20 @@ namespace CollegeApp.Data
 {
     public class CollegeDbContext : DbContext
     {
+        private DbSet<Student> Students { get; set; }
         public CollegeDbContext(DbContextOptions<CollegeDbContext> options) : base(options) 
         {
 
         }
-        private DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Student>().HasData(new List<Student>()
+            {
+                new Student(){Id = 1,StudentName = "mehdi",Address = "Abhar",Email = "mehdi@gmail.com",DOB = new DateTime(2022,12,2)},
+                new Student(){Id = 2,StudentName = "merss",Address = "Rasht",Email = "merss@gmail.com",DOB = new DateTime(2022,10,5)}
+            });
+        }
     }
 }
---
+++ CollegeApp/Migrations/20251015223529_intialValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CollegeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class intialValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Address", "DOB", "Email", "StudentName" },
+                values: new object[,]
+                {
+                    { 1, "Abhar", new DateTime(2022, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "mehdi@gmail.com", "mehdi" },
+                    { 2, "Rasht", new DateTime(2022, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "merss@gmail.com", "merss" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
