b99db3caaa7132246e97f3984b55d6d2888ce71e
Mehdi Heidari <hymehdi84@gmail.com>
2025-10-18 18:17:43 +0200

    automapper error

--- CollegeApp/Program.cs
+++ CollegeApp/Program.cs
@@ -29,9 +29,9 @@ builder.Services.AddControllers(
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddTransient<IMyLogger, LogToServerMemory>();
 builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
+builder.Services.AddTransient<IMyLogger, LogToServerMemory>();
+
 
 var app = builder.Build();
 
