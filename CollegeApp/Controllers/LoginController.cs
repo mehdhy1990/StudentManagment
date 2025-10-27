61390241760e5e85f3091f65532a893e19aa9697
Mehdi Heidari <hymehdi84@gmail.com>
2025-10-27 15:13:37 +0100

    Login controller added and token is allocated

--- CollegeApp/Controllers/LoginController.cs
+++ CollegeApp/Controllers/LoginController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CollegeApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CollegeApp.Controllers
 {
@@ -9,10 +14,43 @@ namespace CollegeApp.Controllers
     [AllowAnonymous]
     public class LoginController : ControllerBase
     {
-        [HttpGet]
-        public string Login()
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public ActionResult Login(LoginDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("please give us username and password");
+            }
 
+            LoginResponseDTO response = new() { UserName = model.UserName };
+            if (model.UserName == "mehdi" && model.Password == "mehdi")
+            {
+                var key = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("JWTSecret"));
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name,model.UserName),
+                        new Claim(ClaimTypes.Role,"Admin")
+                    }),
+                    Expires = DateTime.Now.AddHours(4),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                response.Token = tokenHandler.WriteToken(token);
+            }
+            else
+            {
+                return BadRequest("username and password is not valid");
+            }
+
+            return Ok(response);
         }
     }
 
